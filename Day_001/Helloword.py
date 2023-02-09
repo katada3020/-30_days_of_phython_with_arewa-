82e448aad0648c18a836f0a74822110b0475ce36
katada3020 <117334762+katada3020@users.noreply.github.com>
2023-02-09 18:46:30 +0100

    Create Helloword.py

---
+++ Day_001/Helloword.py
@@ -0,0 +1,54 @@
+#variables
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+print('nasiru') 
+print('ojoko')
+print('nigeria')                   
+print('i am enjoying 30 days of phython')
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type('nasiru'))
+print(type('ojoko'))
+print(type('nigeria'))
+print(5)
+print(9.9)
+print(3+9j)
+print('katada')
+print(['nasir',5,'katada','arewa'])
+#numeric
+x = 10 # integer
+y = 10.5 # float
+z = 4 + 5j # complex
+#string
+
+string = "Hello World"
+#boolean
+Is_married = True
+#list
+list = [1, 2, 3, 4, 5]
+#tuple
+tuple = (1, 2, 3, 4, 5)
+#set
+set = {1, 2, 3, 4, 5}
+#dict
+dict = {
+  "name": "nasiru",
+  "age": 21,
+  "country": "nigeria"
+}
+#finding elucidian distance
+
+x1 = 2
+y1 = 3
+x2 = 10
+y2 = 8
+
+distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(distance)
