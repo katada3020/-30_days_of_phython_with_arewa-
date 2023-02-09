174f23a7d998e003ed473a6e62924538c324259c
katada3020 <117334762+katada3020@users.noreply.github.com>
2023-02-09 18:47:41 +0100

    Create Variable.py

---
+++ Day_002/Variable.py
@@ -0,0 +1,45 @@
+#day2:30 days of phython  programming
+first_name = 'nasiru'
+last_name = 'mohammed'
+full_name = 'mohammed nasiru'
+country = 'nigeria'
+city = 'kaduna'
+age = 25
+year = 2023
+is_married = True
+is_true =  True
+is_light_on = True
+name, level,course = 'yusuf', 200, 'biochemistry'
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_light_on))
+print(type(name))
+print(type(level))
+print(len(first_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+print(total)
+diff = num_one - num_two
+print(diff)
+product = num_one * num_two
+print (product)
+floor_division = num_one//num_two
+print(floor_division)
+#the area and circumference of a circle
+pi =3.142
+r = 30
+area_of_circle = pi * r**2
+print(area_of_circle)
+circum_circle = 2*pi*r**2
+print(circum_circle)
+user_name = input('what is your name :')
+user_lastname = ('what is your lastname: ')
+user_country = ('what is your country?')
+user_age = input('how old are you? ')
