8dd3b671970691fda967cdef23e039983a8e64d6
katada3020 <117334762+katada3020@users.noreply.github.com>
2023-02-10 07:05:27 +0100

    Create Higherfxn .py

---
+++ Day_014/Higherfxn .py
@@ -0,0 +1,20 @@
+#Map, filter, and reduce are three important built-in functions in Python that allow you to manipulate and process data stored in lists.
+#Map, filter, and reduce are three important built-in functions in Python that allow you to manipulate and process data stored in lists.
+#reduce is a function that takes a function and an iterable and returns a single value. It applies the function to the first two elements, then to the result and the next element, and so on, until all elements have been processes
+#A closure is a function object that has access to variables in its enclosing scope even if they are not in memory. 
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+for country in countries:
+    print(country)
+  #using loop to print names
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+for name in names:
+    print(name)
+    #using loop to print numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in numbers:
+    print(number)
+
+
